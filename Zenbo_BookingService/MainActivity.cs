00d8b2d0696d4baa2d00ec0d83008f0f08e2c893
TsaiYork <adidas1101@hotmail.com>
2018-03-20 15:45:44 +0800

    the firsy app can deploy on zenphone also zebo, function :click and count

--- Zenbo_BookingService/MainActivity.cs
+++ Zenbo_BookingService/MainActivity.cs
@@ -1,19 +1,27 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System;
 
 namespace Zenbo_BookingService
 {
     [Activity(Label = "Zenbo_BookingService", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        int count = 1;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            Button button1 = FindViewById<Button>(Resource.Id.button1);
+
+            button1.Click += delegate { button1.Text = string.Format("{0} clicks!", count++); };
         }
+
+  
     }
 }
 
